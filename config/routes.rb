8231e7484f5460b47799223397d0cd30e2d87fbd
Sebhastien Gibosse <Sebhastien@sebhastiens-mbp.attlocal.net>
2016-10-26 22:02:45 -0400

    Fix Javascript/sign out issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+
+  devise_for :users
   mount Payola::Engine => '/payola', as: :payola
   resources :subscriptions
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   resources :courses do
     resources :lessons, :seats, :quizzes
   end
@@ -10,4 +11,5 @@ Rails.application.routes.draw do
 
   root 'main#home'
   get 'dashboard', to: 'main#dashboard'
+  
 end
